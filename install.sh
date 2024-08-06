f84fc31fb61527e07a071eec7bf8aee5f100f703
SBC-ThanhDD <thanhdd@bsscommerce.com>
2024-08-06 21:38:11 +0700

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-echo Start installing!
-# sudo apt update
-
 export Color_Off=$(tput sgr0) # Text Reset
 export Red=$(tput setaf 1)    # Red
 export Green=$(tput setaf 2)  # Green
@@ -9,12 +6,15 @@ export Yellow=$(tput setaf 3) # Yellow
 export Purple=$(tput setaf 5) # Purple
 export Cyan=$(tput setaf 6)   # Cyan
 
+echo "${Cyan}Start installing!${Color_Off}"
+# sudo apt update
+
 # nala
 if ! command -v nala &>/dev/null; then
   echo "Installing nala package manager..."
   sudo apt install nala -y
 else
-  echo "Nala installed"
+  echo "${Yellow}Nala installed${Color_Off}"
 fi
 
 # curl
@@ -24,7 +24,7 @@ then
     # Cài đặt curl
     sudo apt install -y curl
 else
-    echo "Curl installed"
+    echo "${Yellow}Curl installed${Color_Off}"
 fi
 
 
@@ -101,4 +101,55 @@ else
     echo "Spotify installed"
 fi
 
-#config time zone when use dual boot ubuntu and window
+
+# zsh
+
+#!/bin/bash
+
+# Define function to check if a command exists
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+# Check if zsh is installed
+if command_exists zsh; then
+    echo "zsh is already installed"
+else
+    # Install zsh
+    echo "Installing zsh..."
+    sudo nala install zsh -y
+
+    # Change default shell to zsh
+    chsh -s $(which zsh)
+fi
+
+# Check if Oh My Zsh is installed
+if [ -d "$HOME/.oh-my-zsh" ]; then
+    echo "Oh My Zsh is already installed"
+else
+    # Install Oh My Zsh
+    echo -e "${Green}--------Installing zsh and oh-my-zsh--------${Color_Off}"
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+fi
+
+# Check again if Oh My Zsh is installed
+if [ -d "$HOME/.oh-my-zsh" ]; then
+    # Install plugins
+    echo -e "${Green}--------Installing zsh plugins--------${Color_Off}"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
+    # Update .zshrc to include the new plugins
+    sed -i -E 's,^plugins=\(.*$,plugins=(git zsh-syntax-highlighting zsh-autosuggestions zsh-history-substring-search z nvm),g' ~/.zshrc
+fi
+
+echo "Installation complete. Please restart your shell or run 'exec zsh' to start using zsh."
+
+
+# gnome extension
+sudo apt install gnome-shell-extension-manager -y
+
+# Fix time zone when dual boot
+timedatectl set-local-rtc 1 --adjust-system-clock
+timedatectl
